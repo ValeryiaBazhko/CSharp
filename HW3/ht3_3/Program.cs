62490f31e2929e90b2913a252fd9c02812faa49b
ValeryiaBazhko <lerabozhkozzz@gmail.com>
2023-02-23 00:51:15 +0100

    third task(thd ht) is done

---
+++ HW3/ht3_3/Program.cs
@@ -0,0 +1,37 @@
+﻿int main(string message){
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int[] Cubes(int num){
+    int[] cubes = new int[num];
+
+    for (int i = 1; i <= num; i++){
+        cubes[i-1] = Convert.ToInt32(Math.Pow(i, 3));
+    }
+    
+    for (int i = 0; i < cubes.Length; i++){
+        Console.Write($"{cubes[i]} ");
+    }
+
+    return cubes;
+}
+
+bool IfValid(int num){
+    if(num > 0){
+        return true;
+    }
+    
+    return false;
+}
+
+int num = main("Enter the number: ");
+
+if(IfValid(num)){
+    int[] result = Cubes(num);
+} else {
+    Console.Write("Incorrect input");
+}
+
+
