a63ef37d1932e0ffdbda76f49aaf4ba6c1e40046
ValeryiaBazhko <lerabozhkozzz@gmail.com>
2023-02-22 23:57:55 +0100

    first task(thd ht) is done

---
+++ HW3/ht3_1/Program.cs
@@ -0,0 +1,29 @@
+﻿int Main(string message){
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+bool IsPalindrom(int num){
+    int reverse = 0;
+    int pal = num;
+
+    while(pal != 0){
+        int rem = pal % 10;
+        reverse = reverse * 10 + rem;
+        pal = pal / 10;
+    }
+
+    if (num == reverse){
+        return true;
+    }
+    return false;
+}
+
+int num = Main("Enter the number: ");
+
+if(IsPalindrom(num)){
+    Console.Write("Yes");
+} else {
+    Console.Write("No");
+}
