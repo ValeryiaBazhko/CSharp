46af0fbb60a17c283d19fd9448b65b885d6f148a
ValeryiaBazhko <lerabozhkozzz@gmail.com>
2023-02-22 22:33:01 +0100

    second task done

---
+++ ht2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter three numbers: ");
+List<int> nums = new List<int>();
+for(int i = 0; i < 3; i++){
+    nums.Add(Convert.ToInt32(Console.ReadLine()));
+}
+
+int max = 0;
+for (int i = 0; i < nums.Count; i++){
+    if (nums[i] > max){
+        max = nums[i];
+    }
+}
+Console.Write($"Max is: {max}");
