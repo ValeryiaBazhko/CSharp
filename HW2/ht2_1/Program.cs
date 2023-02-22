860542bd143e397d13593e147ce4c112fa9bf80d
ValeryiaBazhko <lerabozhkozzz@gmail.com>
2023-02-22 23:14:17 +0100

    first task(snd ht) is done2

--- HW2/ht2_1/Program.cs
+++ HW2/ht2_1/Program.cs
@@ -4,12 +4,10 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 int two_digit = 0;
 int second_digit = 0;
-
 if(num > 99 && num < 1000){
-    two_digit = num/10;
+    two_digit = num / 10;
     second_digit = two_digit % 10;
     Console.Write($"{second_digit}");
-} else {
-    Console.WriteLine("Incorrect input!");
+} else{
+    Console.Write("Incorrect input!");
 }
-
