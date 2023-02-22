c996dc44fa3c52c99d4b493d21932385c287ed85
ValeryiaBazhko <lerabozhkozzz@gmail.com>
2023-02-22 23:34:35 +0100

    second task(snd ht) is done2

---
+++ HW2/ht2_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter the number: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int third_digit = 0;
+if(num < 100){
+    Console.Write("Third digit doesnt exist");
+} else {
+    while(num > 999){
+        num /= 10;
+    }
+
+    third_digit = num % 10;
+    Console.Write($"{third_digit}");
+}
