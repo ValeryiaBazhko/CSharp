8e49b8ad07b8829d4f70250eb7ec6ef77f7df6b2
ValeryiaBazhko <lerabozhkozzz@gmail.com>
2023-02-22 22:21:15 +0100

    first task is done

---
+++ ht1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b){
+    Console.WriteLine($"max: {a}");
+}
+
+else {
+    Console.WriteLine($"max: {b}");
+}
--- task0/Program.cs
+++ task0/Program.cs
@@ -1,7 +1,1 @@
-﻿Console.Write("Enter the number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-int square = 0;
-
-square = number*number;
-Console.Write($"Your square of the {number} = {square}");
+﻿
