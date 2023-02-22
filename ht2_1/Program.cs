0565b9982e9affeb96e1705978afe96797fe0499
ValeryiaBazhko <lerabozhkozzz@gmail.com>
2023-02-22 22:52:28 +0100

    first task(snd ht) is done

---
+++ ht2_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter three-digit number: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int two_digit = 0;
+int second_digit = 0;
+if(num > 99 && num < 1000){
+    two_digit = num / 10;
+    second_digit = two_digit % 10;
+}
+
+Console.Write($"{second_digit}");
