b0f5b03cecada63e72c656318f8c59a937a1f701
ValeryiaBazhko <lerabozhkozzz@gmail.com>
2023-02-22 22:36:30 +0100

    third task is done

---
+++ ht3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter the number: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0){
+    Console.WriteLine("Yes");
+} else{
+    Console.WriteLine("No");
+
+}
