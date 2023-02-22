d3064198be56437174c7a6044a06c7fd0a2534ec
ValeryiaBazhko <lerabozhkozzz@gmail.com>
2023-02-22 23:39:38 +0100

    third task(snd ht) is done2

---
+++ HW2/ht2_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter the number of the day in a week: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num < 1 || num > 7){
+    Console.Write("Incorrect input.");
+} else {
+    if(num == 6 || num == 7 ){
+        Console.Write("Yes");
+    } else {
+        Console.Write("No");
+    }
+}
