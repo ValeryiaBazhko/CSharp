bcd93c5efd09aaec89586bd7c2f4e3569e4b9028
ValeryiaBazhko <lerabozhkozzz@gmail.com>
2023-03-09 23:59:18 +0100

    7th ht 2nd task

---
+++ HW7/ht7_2/Program.cs
@@ -0,0 +1,33 @@
+﻿string Main(string message){
+    Console.Write(message);
+    string num = Console.ReadLine();
+    return num;
+}
+
+void ifExists(string nums){
+    string[] input = nums.Split(" "); 
+    int a = int.Parse(input[0]);
+    int b = int.Parse(input[1]);
+    int[,] matrix = new int[6,6];
+
+    Random ran = new Random();
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            matrix[i,j] = (ran.Next(1,10));
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+
+    if(ifValid(a,b) && a < matrix.GetLength(0) && b < matrix.GetLength(1)){
+        Console.Write($"The value of matrix at the position [{a},{b}] is: " + matrix[a,b]);
+    } else {
+        Console.Write("Such element doesnt exist");
+    }
+}
+
+bool ifValid(int a, int b){
+    return (a >= 0 && a < 6 && b >= 0 && b < 6);
+}
+
+ifExists(Main("Enter the position of the element you want to find: "));
