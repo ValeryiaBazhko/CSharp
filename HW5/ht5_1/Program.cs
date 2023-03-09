772b80512e16e84c59704fde9d107a8cb95e65b0
ValeryiaBazhko <lerabozhkozzz@gmail.com>
2023-03-10 00:45:06 +0100

    5th ht 1st task

---
+++ HW5/ht5_1/Program.cs
@@ -0,0 +1,34 @@
+﻿string Main(string message){
+    Console.Write(message);
+    string num = Console.ReadLine();
+    return num;
+}
+
+int[] makeArray(string nums){
+    int input = Convert.ToInt32(nums); 
+    int[] arr = new int[input]; 
+
+    Random ran = new Random();
+    for(int i = 0; i < input; i++){
+        arr[i] = ran.Next(1, 1001);
+        Console.Write(arr[i] + " ");
+    }
+    
+    return arr;
+}
+
+int even(int[] arr){
+    int num = 0;
+    for(int i = 0; i < arr.Length; i++){
+        if(arr[i] % 2 == 0){
+            num++;
+        }
+    }
+    Console.WriteLine("");
+    Console.Write("Number of even numbers in the array is: " + num);
+    return num;
+}
+
+int nums = even(makeArray(Main("Enter the number of elts in the array: ")));
+
+
