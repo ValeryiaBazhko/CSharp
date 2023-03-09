fcbd9816b6bbee4167085a836d4f51f66d73756e
ValeryiaBazhko <lerabozhkozzz@gmail.com>
2023-03-09 23:21:49 +0100

    7th ht 1st task

---
+++ HW4/ht4_1/Program.cs
@@ -0,0 +1,23 @@
+﻿string Main(string message){
+    Console.Write(message);
+    string num = Console.ReadLine();
+    return num;
+}
+
+int Power(string num){
+    string[] input = num.Split(" ");
+    int a = int.Parse(input[0]);
+
+    int b = int.Parse(input[1]);
+
+    int power = 1;
+
+    for(int i = 0; i < b; i++){
+        power *= a;
+    }
+
+    Console.Write($"Power of {a} in the exp {b} is: {power}");
+    return power;
+}
+
+int power = Power(Main("Enter the base and the exp in the format: "));
---
+++ HW7/ht7_1/Program.cs
@@ -0,0 +1,26 @@
+﻿string Main(string message){
+    Console.Write(message);
+    string num = Console.ReadLine();
+    return num;
+}
+
+double[,] makeMatrix(string nums){
+    string[] input = nums.Split(" "); 
+    int a = int.Parse(input[0]);
+    int b = int.Parse(input[1]);
+    double[,] matrix = new double[a,b];
+
+    Random ran = new Random();
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            matrix[i,j] = (ran.NextDouble() * (5 -(-5)));
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+    
+    return matrix;
+}
+
+double[,] matrix = makeMatrix(Main("Enter dimensions of the matrix: "));
+
