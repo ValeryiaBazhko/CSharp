c7681187cdabea79fc341ee9d2df40029e58b33f
ValeryiaBazhko <lerabozhkozzz@gmail.com>
2023-03-14 17:47:21 +0100

    9th ht 1st task

---
+++ HW9/ht9_1/Program.cs
@@ -0,0 +1,25 @@
+﻿int main(string message){
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void Print(int num){
+    if(ifValid(num)){
+        if(num == 1){
+            Console.Write("1");
+        } else {
+            Console.Write($"{num}, ");
+            Print(num-1);
+        }
+    }
+}
+
+bool ifValid(int num){
+    if(num > 0){
+        return true;
+    }
+    return false;
+}
+
+Print(main("Enter the number: "));
