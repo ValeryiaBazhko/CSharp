ebac01bc27bfd0b8fa953efbe253e62634f0495c
ValeryiaBazhko <lerabozhkozzz@gmail.com>
2023-03-14 18:03:54 +0100

    9th ht 2nd task

---
+++ HW9/ht9_2/Program.cs
@@ -0,0 +1,27 @@
+﻿
+void Main(string message) {
+    Console.Write(message);
+    string[] input = Console.ReadLine().Split(" "); 
+    int m = int.Parse(input[0]);
+    int n = int.Parse(input[1]);
+        
+    if (ifValid(m,n)) {
+        Console.WriteLine($"Sum of numbers between {m} and {n} is: {SumNumbers(m,n)}");
+    } else {
+        Console.WriteLine("Invalid input: m must be less than or equal to n.");
+    }
+}
+
+int SumNumbers(int m, int n) {
+    if (m == n) {
+        return m;
+    } else {
+        return n + SumNumbers(m, n-1);
+    } 
+}
+    
+bool ifValid(int m, int n) {
+    return m <= n;
+}
+
+Main("Enter m and n: ");
